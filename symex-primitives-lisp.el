fb2c296bcd39a9abdef452fbc91da11d2cd0403c
Siddhartha <sid@countvajhula.com>
2023-05-30 13:21:22 -0700

    restore correct(?) default handling of tidying after deletion

--- symex-primitives-lisp.el
+++ symex-primitives-lisp.el
@@ -673,7 +673,8 @@ line."
                                (save-excursion (evil-find-char 1 ?\;)
                                                t)
                              (error nil))
-                     (symex--join-to-match symex--re-right))))))))))
+                     (symex--join-to-match symex--re-right))))))))
+        (t (symex--join-to-non-whitespace))))
 
 ;;; Utilities
 
